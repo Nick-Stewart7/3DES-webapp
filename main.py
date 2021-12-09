505909f93595d6cfec167e38be7fc0028c844147
Nick-Stewart7 <nsstewart7@gmail.com>
2021-12-09 14:13:26 -0500

    added code to handle DES, 2DES and 3DES with given keys

--- main.py
+++ main.py
@@ -22,10 +22,16 @@ def start_Page():
 def encrypt():
     message = request.form['enctext']
     messageHex = ''
+    keyList = []
     key1 = request.form['key1']
+    if key1 != '':
+        keyList.append(key1)
     key2 = request.form['key2']
+    if key2 != '':
+        keyList.append(key2)
     key3 = request.form['key3']
-    keyList = [key1, key2, key3]
+    if key3 != '':
+        keyList.append(key3)
     #turn message into hex by character, remove 0x, make one large string
     for character in message:
         element = hex((ord(character)))
@@ -58,21 +64,37 @@ def encrypt():
 
 @app.route('/decrypt/', methods=['POST'])
 def decrypt():
+    keyList = []
     message = request.form['dectext']
     key1 = request.form['key1']
+    if key1 != '':
+        keyList.append(key1)
     key2 = request.form['key2']
+    if key2 != '':
+        keyList.append(key2)
     key3 = request.form['key3']
-    keyList = [key1, key2, key3]
+    if key3 != '':
+        keyList.append(key3)
     #format message to be feed into the decrypt
     cipherBlocks = message.split("0x")
     cipherBlocks.remove('')
     cipherBlocks = list(map(lambda x: int(x, base=16), cipherBlocks))
     #perform 3 DES dec with reversed keys
-    for i in range(0, len(keyList)):
-        dec = startDecrpyt(cipherBlocks, keyList[2-i])
-        halfBlocks = list(map(lambda x: int(x, base=16), dec))
-        cipherBlocks = halfBlocks
-    clearHex = list(map(lambda x: x[2:], dec))
+    if len(keyList) == 1:
+        dec = startDecrpyt(cipherBlocks, keyList[0])
+        clearHex = list(map(lambda x: x[2:], dec))
+    elif len(keyList) == 2:
+        for i in range(0, len(keyList)):
+            dec = startDecrpyt(cipherBlocks, keyList[1-i])
+            halfBlocks = list(map(lambda x: int(x, base=16), dec))
+            cipherBlocks = halfBlocks
+        clearHex = list(map(lambda x: x[2:], dec))
+    elif len(keyList) == 3:
+        for i in range(0, len(keyList)):
+            dec = startDecrpyt(cipherBlocks, keyList[2-i])
+            halfBlocks = list(map(lambda x: int(x, base=16), dec))
+            cipherBlocks = halfBlocks
+        clearHex = list(map(lambda x: x[2:], dec))
     #format to display
     decodetext = ''
     for clear in clearHex: 
